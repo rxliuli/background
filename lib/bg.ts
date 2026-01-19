bfd87a2f0e7e396e2aab80bf2448fa7d5d54df29
rxliuli <rxliuli@gmail.com>
2026-01-19 14:47:19 +0800

    fix: fixed ios render bug

--- lib/bg.ts
+++ lib/bg.ts
@@ -4,18 +4,21 @@ export function renderBackgroundStyle(bg: string) {
   removeBackgroundStyle()
   const style = document.createElement('style')
   style.id = STYLE_ID
+  // TODO: iOS Safari bug https://www.reddit.com/r/csshelp/comments/1cefzv4/comment/l1ji6kp/
   style.textContent = `html::before {
     content: "";
     position: fixed;
     top: 0;
     left: 0;
-    width: 100%;
-    height: 100%;
+    right: 0;
+    bottom: 0;
+    width: 100vw;
+    height: 100dvh;
     z-index: ${Number.MAX_SAFE_INTEGER};
     background-image: url('${bg}');
     background-size: cover;
     background-position: center;
-    background-attachment: fixed;
+    background-attachment: scroll;
     opacity: 0.1;
     pointer-events: none;
 }`
